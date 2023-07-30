c4b34501323b16c8163fcd8a852f9c482f442593
wangtao <1498073191@qq.com>
2023-07-30 15:43:59 +0800

    server.go 还需要设计

--- server.go
+++ server.go
@@ -1,13 +1,10 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-)
-
 func main() {
-	r := gin.Default()
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.String(200, "Hello World")
-	})
-	r.Run()
+
+}
+
+func Init(filePath string) error {
+
+	return nil
 }
