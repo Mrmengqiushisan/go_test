a2461a8186549b4e5be098bb24de898203e93934
wangtao <1498073191@qq.com>
2023-07-30 20:29:23 +0800

    实现了贴子的更新输入

--- repository/db_init.go
+++ repository/db_init.go
@@ -9,14 +9,28 @@ import (
 
 var (
 	topicIndexMap map[int64]*Topic
+	topicId       int64
 	postIndexMap  map[int64][]*Post
+	postId        int64
 )
 
+func GetPostCount() *int64 {
+	return &postId
+}
+func GetTopicCount() *int64 {
+	return &topicId
+}
+
 func InitTopicIndexMap(filepath string) error {
+	if len(topicIndexMap) > 0 {
+		topicIndexMap = make(map[int64]*Topic)
+		topicId = 0
+	}
 	open, err := os.Open(filepath + "topic") //open os流handle
 	if err != nil {
 		return err
 	}
+	defer open.Close()
 	scanner := bufio.NewScanner(open)
 	topicTmpMap := make(map[int64]*Topic)
 	for scanner.Scan() {
@@ -32,16 +46,22 @@ func InitTopicIndexMap(filepath string) error {
 		}
 		fmt.Println(string(buf))
 		topicTmpMap[topic.Id] = &topic
+		topicId++
 	}
 	topicIndexMap = topicTmpMap
 	return nil
 }
 
 func InitPostIndexMap(filepath string) error {
+	if len(postIndexMap) > 0 {
+		postIndexMap = make(map[int64][]*Post)
+		postId = 0
+	}
 	open, err := os.Open(filepath + "post")
 	if err != nil {
 		return err
 	}
+	defer open.Close()
 	scanner := bufio.NewScanner(open)
 	posTmpMap := make(map[int64][]*Post)
 	for scanner.Scan() {
@@ -53,16 +73,20 @@ func InitPostIndexMap(filepath string) error {
 		post, ok := posTmpMap[pos.ParentId]
 		if !ok {
 			posTmpMap[pos.ParentId] = []*Post{&pos}
+			postId++
 			continue
 		}
 		post = append(post, &pos)
 		posTmpMap[pos.ParentId] = post
+		postId++
 	}
 	postIndexMap = posTmpMap
 	return nil
 }
 
 func Init(filepath string) error {
+	topicId = 0
+	postId = 0
 	if err := InitTopicIndexMap(filepath); err != nil {
 		return err
 	}
--- server.go
+++ server.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/Mrmengqiushisan/go_test/cotroller"
 	"github.com/Mrmengqiushisan/go_test/repository"
@@ -12,9 +19,32 @@ func main() {
 	if err := Init("./data/"); err != nil {
 		os.Exit(-1)
 	}
+	fmt.Println("话题数为：", *repository.GetTopicCount())
+	fmt.Println("贴子数为：", *repository.GetPostCount())
+
 	r := gin.Default()
 	r.GET("/community/page/get/:id", func(ctx *gin.Context) {
 		topicId := ctx.Param("id")
+		for {
+			fmt.Println("请问是否有帖子需要输入呢")
+			reader := bufio.NewReader(os.Stdin)
+			put, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("input error", err)
+				break
+			}
+			putstr := strings.Trim(put, "\r\n")
+			if strings.ToUpper(putstr) == "Y" {
+				if err := InputPost(); err != nil {
+					break
+				}
+			} else {
+				break
+			}
+		}
+		if err := Init("./data/"); err != nil {
+			fmt.Println("再次初始化失败", err)
+		}
 		data := cotroller.QueryPageInfo(topicId)
 		ctx.JSON(200, data)
 	})
@@ -27,6 +57,65 @@ func main() {
 func Init(filePath string) error {
 	if err := repository.Init(filePath); err != nil {
 		return err
+	}
+	return nil
+}
+
+func InputPost() error {
+	for {
+		fmt.Println("结束请输入E")
+		fmt.Println("我需要以下信息：")
+		fmt.Println("请输入发布帖子的内容：")
+		reader := bufio.NewReader(os.Stdin)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("An error occured while reading input Please try again", err)
+			continue
+		}
+		topicContext := strings.Trim(input, "\r\n")
+		if strings.ToUpper(topicContext) == "E" {
+			break
+		}
+		fmt.Println("请输入您选择的话题：")
+		reader = bufio.NewReader(os.Stdin)
+		input, err = reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("An error occured while reading input Please try again", err)
+			continue
+		}
+		parentIdStr := strings.Trim(input, "\r\n")
+		parent, err := strconv.ParseInt(parentIdStr, 10, 64)
+		if err != nil {
+			fmt.Println("Invalid input,Please enter an integer value")
+			continue
+		} //构建帖子结构
+		now := time.Now()
+		id := repository.GetPostCount()
+		(*id)++
+		fmt.Println("当前id值为")
+		posTmp := repository.Post{
+			Id:         *id,
+			ParentId:   parent,
+			Content:    topicContext,
+			CreateTime: now.Unix(),
+		}
+		open, err := os.OpenFile("./data/post", os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			fmt.Println("file can not open ")
+			return errors.New("file can not open")
+		}
+		buf, err := json.Marshal(posTmp)
+		if err != nil {
+			fmt.Println("序列化失败")
+			return err
+		}
+		_, err = open.Write([]byte{'\n'})
+		_, err = open.Write(buf)
+		if err != nil {
+			fmt.Println("写入失败", err)
+			return err
+		}
+		fmt.Println("数据写入成功")
 	}
 	return nil
 }
